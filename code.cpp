ab0ca4a0cdb0ba7e6c02ca756f14221919ce181e
Usman-Khan2 <157693839+Usman-Khan2@users.noreply.github.com>
2024-01-25 15:02:54 +0500

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x, y;
+
+    cout <<"Enter 1st number:" <<endl;
+    cin >> x;
+
+    cout <<"Enter 2nd number:" <<endl;
+    cin >> y;
+
+    cout <<"Sum:" <<x+y <<endl;
+    cout <<"Difference:" <<x-y <<endl;
+    cout <<"Product:" << x*y <<endl;
+    return 0;
+}
